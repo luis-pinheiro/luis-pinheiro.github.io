1babe1f4cb62f382bfa816b5e762e0e411781c5c
Luis Pinheiro <profitcreations@live.com>
2017-05-30 16:00:29 +0100

    Added jumbotron

--- dist/js/index.js
+++ dist/js/index.js
@@ -1,5 +1,5 @@
 
-$(document).ready(function(){$('.nav-link').click(function(){$('#navbarNav1').removeClass('show');});Tu.t_scroll({'t_element':".title .slideDown",'t_duration':0.1,'t_delay':0.5,'t_function':'ease-in'});Tu.t_scroll({'t_element':'.title_2 .slideDown','t_duration':1,'t_delay':2,'t_function':'ease-in'})
+$(document).ready(function(){$('.nav-link').click(function(){$('#navbarNav1').removeClass('show');});Tu.t_scroll({'t_element':".display-3 .slideDown",'t_duration':0.1,'t_delay':0.5,'t_function':'ease-in'});Tu.t_scroll({'t_element':'.title_2 .slideDown','t_duration':1,'t_delay':2,'t_function':'ease-in'})
 Tu.t_scroll({'t_element':'.title_2 .slideRight','t_duration':1,'t_delay':2,'t_function':'ease-in'})
 Tu.t_scroll({'t_element':'.title_2 .slideLeft','t_duration':1,'t_delay':2,'t_function':'ease-in'})
 Tu.t_scroll({'t_element':'.social .bounceIn','t_duration':0.2,'t_delay':3,'t_function':'ease-in'})
