8eae3181b2310f893fa30126ff81da55db635685
Luis Pinheiro <profitcreations@live.com>
2017-05-31 23:15:25 +0100

    Changed about section

--- gulpfile.js
+++ gulpfile.js
@@ -10,35 +10,38 @@ var rucksack = require('gulp-rucksack');
 var htmlbeautify = require('gulp-html-beautify');
 var imagemin = require('gulp-imagemin');
 
-gulp.task('img', function () {
-	return gulp.src('./src/img/**.*')
-	.pipe(imagemin())
-	.pipe(gulp.dest('./dist/img'));
+gulp.task('img', function() {
+    return gulp.src('./src/img/**.*')
+        .pipe(imagemin())
+        .pipe(gulp.dest('./dist/img'));
 });
 
 gulp.task("html", function() {
-	var options = {
-		"indent_size": 4
-	}
-	return gulp
-		.src("src/index.html")
-		.pipe(htmlbeautify(options))
-		.pipe(gulp.dest("."))
-		.pipe(browserSync.stream());
+    var options = {
+        "indent_size": 4
+    }
+    return gulp
+        .src("src/index.html")
+        .pipe(htmlbeautify(options))
+        .pipe(gulp.dest("."))
+        .pipe(browserSync.stream());
 });
 
 gulp.task("sass", function() {
     return gulp
-        .src("src/scss/*.scss")
+        .src("src/scss/style.scss")
         .pipe(sourcemaps.init())
         .pipe(sass())
         .pipe(cssmin())
-				.pipe(rucksack())
+        .pipe(rucksack({
+            clearFix: false,
+            fallbacks: true
+        }))
         .pipe(autoprefixer({
             browsers: ['last 2 versions'],
             cascade: true
         }))
-				.pipe(cssnano())
+        .pipe(cssnano())
         .pipe(sourcemaps.write("."))
         .pipe(gulp.dest("./dist/css"))
         .pipe(browserSync.stream());
@@ -57,8 +60,8 @@ gulp.task("serve", ["html", "js", "sass", 'img'], function() {
 
 gulp.task("js", function() {
     return gulp.src("src/js/*.js")
-		.pipe(jsmin())
-		.pipe(gulp.dest("dist/js"));
+        .pipe(jsmin())
+        .pipe(gulp.dest("dist/js"));
 });
 
-gulp.task("default", [ "serve"]);
+gulp.task("default", ["serve"]);
