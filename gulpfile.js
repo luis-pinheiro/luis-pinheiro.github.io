f703498c61e9d5f3c7aacc31f5d0fb80abb3e592
Luis Pinheiro <profitcreations@live.com>
2017-06-13 15:42:02 +0100

    Fixed scss

--- gulpfile.js
+++ gulpfile.js
@@ -3,79 +3,82 @@ var browserSync = require("browser-sync").create();
 var sass = require("gulp-sass");
 var sourcemaps = require("gulp-sourcemaps");
 var autoprefixer = require("gulp-autoprefixer");
-var cssmin = require('gulp-cssmin');
-var jsmin = require('gulp-jsmin');
-var cssnano = require('gulp-cssnano');
-var rucksack = require('gulp-rucksack');
-var htmlbeautify = require('gulp-html-beautify');
-var imagemin = require('gulp-imagemin');
-var runSequence = require('run-sequence');
+var cssmin = require("gulp-cssmin");
+var jsmin = require("gulp-jsmin");
+var cssnano = require("gulp-cssnano");
+var rucksack = require("gulp-rucksack");
+var htmlbeautify = require("gulp-html-beautify");
+var imagemin = require("gulp-imagemin");
+var runSequence = require("run-sequence");
 
-gulp.task('img', function() {
-    return gulp.src('./src/img/**.*')
-        .pipe(imagemin())
-        .pipe(gulp.dest('./dist/img'));
+gulp.task("img", function() {
+  return gulp
+    .src("./src/img/**.*")
+    .pipe(imagemin())
+    .pipe(gulp.dest("./dist/img"));
 });
 
 gulp.task("html", function() {
-    var options = {
-        "indent_size": 4
-    }
-    return gulp
-        .src("src/index.html")
-        .pipe(htmlbeautify(options))
-        .pipe(gulp.dest("."))
-        .pipe(browserSync.stream());
+  var options = {
+    indent_size: 4
+  };
+  return gulp
+    .src("src/index.html")
+    .pipe(htmlbeautify(options))
+    .pipe(gulp.dest("."))
+    .pipe(browserSync.stream());
 });
 
 gulp.task("sass", function() {
-    return gulp
-        .src("src/scss/style.scss")
-        .pipe(sourcemaps.init())
-        .pipe(sass())
-        // .pipe(cssmin())
-        .pipe(rucksack())
-        // .pipe(autoprefixer({
-        //     browsers: ['last 2 versions'],
-        //     cascade: true
-        // }))
-        // .pipe(cssnano())
-        .pipe(sourcemaps.write("."))
-        .pipe(gulp.dest("./dist/css"))
-        .pipe(browserSync.stream());
+  return gulp
+    .src("src/scss/style.scss")
+    .pipe(sourcemaps.init())
+    .pipe(sass())
+    .pipe(cssmin())
+    .pipe(rucksack())
+    .pipe(
+      autoprefixer({
+        browsers: ["last 2 versions"],
+        cascade: true
+      })
+    )
+    .pipe(cssnano())
+    .pipe(sourcemaps.write("."))
+    .pipe(gulp.dest("./dist/css"))
+    .pipe(browserSync.stream());
 });
 
-gulp.task("serve", ["html", "js", "sass", 'img', 'copy-roboto', 'copy-icomoon'], function() {
+gulp.task(
+  "serve",
+  ["html", "js", "sass", "img", "copy-roboto", "copy-icomoon"],
+  function() {
     browserSync.init({
-        server: {
-            baseDir: "."
-        }
+      server: {
+        baseDir: "."
+      }
     });
     gulp.watch("src/scss/**/*.scss", ["sass"]);
     gulp.watch("src/js/*.js", ["js"]).on("change", browserSync.reload);
     return gulp.watch("./*.html").on("change", browserSync.reload);
-});
+  }
+);
 
 gulp.task("js", function() {
-    return gulp.src("src/js/*.js")
-        .pipe(jsmin())
-        .pipe(gulp.dest("dist/js"));
+  return gulp.src("src/js/*.js").pipe(jsmin()).pipe(gulp.dest("dist/js"));
 });
 
-gulp.task('copy-roboto', function() {
-    gulp.src("node_modules/mdbootstrap/font/roboto/*")
-        .pipe(gulp.dest('dist/font/roboto'));
+gulp.task("copy-roboto", function() {
+  gulp
+    .src("node_modules/mdbootstrap/font/roboto/*")
+    .pipe(gulp.dest("dist/font/roboto"));
 });
 
-gulp.task('copy-icomoon', function() {
-    gulp.src('src/font/icomoon/fonts/*')
-        .pipe(gulp.dest('dist/css/fonts'));
+gulp.task("copy-icomoon", function() {
+  gulp.src("src/font/icons/fonts/*").pipe(gulp.dest("dist/css/fonts"));
 });
 
-
-
-gulp.task('del', require('del').bind(null, ['dist']));
+gulp.task("del", require("del").bind(null, ["dist"]));
 
 gulp.task("default", function() {
-    runSequence('del', 'serve');
+  runSequence("del", "serve");
 });
